9a9d6a1542a9fe572e73c251bef19ec052bd6db6
mangeshpoojan <mangeshpoojan@gmail.com>
2026-04-08 16:45:08 +0530

    added option to have multiple dynamic layers

--- PA3.cu
+++ PA3.cu
@@ -1,4 +1,3 @@
-%%cuda -c "--gpu-architecture $gpu_arch"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -12,11 +11,14 @@
 #define BATCH_SIZE 32
 #define OUTPUT_VEC 32
 #define STREAMS 4
+#define LAYERS 4
+#define LEARNING_RATE 0.001f
  
-__global__ void matmul_gpu_shared_mem_tiling(float *A, float *B, float *C, int m, int k, int n){
+__global__ void matmul_gpu_shared_mem_tiling(float *A, float *B, float *C, float *bias, int m, int k, int n){
 
     __shared__ float tileA [BLOCK_SIZE * BLOCK_SIZE];
     __shared__ float tileB [BLOCK_SIZE * BLOCK_SIZE];
+    __shared__ float tileBias[BLOCK_SIZE];
 
     int localRow = threadIdx.y; 
     int localCol = threadIdx.x;  
@@ -27,7 +29,9 @@ __global__ void matmul_gpu_shared_mem_tiling(float *A, float *B, float *C, int m
 
     int numTiles = (k + BLOCK_SIZE - 1)/BLOCK_SIZE;
 
-    
+    if(localRow == 0) tileBias[localCol] = (globalCol < n) ? bias[globalCol] : 0.0f;
+
+    __syncthreads();
 
     for(int i = 0; i < numTiles; i++){
 
@@ -51,20 +55,20 @@ __global__ void matmul_gpu_shared_mem_tiling(float *A, float *B, float *C, int m
     }
 
     if(globalRow < m && globalCol < n){
-        C[globalRow * n + globalCol] = sum;
+        C[globalRow * n + globalCol] = sum + tileBias[localCol];
     }
 
 }
 
-__global__ void add_bias(float *A, float *B, int n, int m){
+// __global__ void add_bias(float *A, float *B, int n, int m){
 
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
-    if(col < m){
-        for (int i = 0; i < n; i++){
-            A[i * m + col] += B[col];
-        }
-    }
-}
+//     int col = blockIdx.x * blockDim.x + threadIdx.x;
+//     if(col < m){
+//         for (int i = 0; i < n; i++){
+//             A[i * m + col] += B[col];
+//         }
+//     }
+// }
 
 __global__ void relu_inplace(float *A, int size){
 
@@ -88,52 +92,64 @@ double get_time() {
     return ts.tv_sec + ts.tv_nsec * 1e-9;
 }
 
-void launch_mlp_chunk(float *d_ip_chunk, float *d_w1,float *d_b1,float *d_op1_chunk, float *d_w2, float *d_b2, float *d_op2_chunk, int batch_size, cudaStream_t stream){
+void launch_mlp_chunk(float *d_ip_chunk, float **d_w, float **d_b, float **d_op_chunk,
+                      int batch_size, cudaStream_t stream){
 
-    //layer1
-    dim3 blockDim1_1(BLOCK_SIZE, BLOCK_SIZE);
-    dim3 gridDim1_1((N + BLOCK_SIZE - 1)/BLOCK_SIZE, (batch_size + BLOCK_SIZE - 1)/BLOCK_SIZE);
-    
-    matmul_gpu_shared_mem_tiling<<<gridDim1_1, blockDim1_1,0,stream>>>(d_ip_chunk, d_w1, d_op1_chunk, batch_size ,INPUT_VEC, N);  
+    dim3 matmul_block(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 hidden_grid((N + BLOCK_SIZE - 1) / BLOCK_SIZE,
+                     (batch_size + BLOCK_SIZE - 1) / BLOCK_SIZE);
+    dim3 output_grid((OUTPUT_VEC + BLOCK_SIZE - 1) / BLOCK_SIZE,
+                     (batch_size + BLOCK_SIZE - 1) / BLOCK_SIZE);
+    dim3 relu_block(BLOCK_SIZE);
+    dim3 relu_grid((batch_size * N + BLOCK_SIZE - 1) / BLOCK_SIZE);
 
-    dim3 blockDim1_2(BLOCK_SIZE);
-    dim3 gridDim1_2((N + BLOCK_SIZE - 1)/BLOCK_SIZE);
+    matmul_gpu_shared_mem_tiling<<<hidden_grid, matmul_block, 0, stream>>>(
+        d_ip_chunk, d_w[0], d_op_chunk[0], d_b[0], batch_size, INPUT_VEC, N);
+    relu_inplace<<<relu_grid, relu_block, 0, stream>>>(d_op_chunk[0], batch_size * N);
 
-    add_bias<<<gridDim1_2, blockDim1_2, 0, stream>>>(d_op1_chunk, d_b1, batch_size, N);
+    for(int i = 1; i < LAYERS - 1; i++){
+        matmul_gpu_shared_mem_tiling<<<hidden_grid, matmul_block, 0, stream>>>(
+            d_op_chunk[i - 1], d_w[i], d_op_chunk[i], d_b[i], batch_size, N, N);
+        relu_inplace<<<relu_grid, relu_block, 0, stream>>>(d_op_chunk[i], batch_size * N);
+    }
 
-    dim3 blockDim1_3(BLOCK_SIZE);
-    dim3 gridDim1_3((batch_size * N + BLOCK_SIZE - 1)/BLOCK_SIZE);
-
-    relu_inplace<<<gridDim1_3, blockDim1_3, 0, stream>>>(d_op1_chunk, batch_size * N);
-
-    // layer2
-    dim3 blockDim2_1(BLOCK_SIZE, BLOCK_SIZE);
-    dim3 gridDim2_1((OUTPUT_VEC + BLOCK_SIZE - 1)/BLOCK_SIZE, (batch_size + BLOCK_SIZE - 1)/BLOCK_SIZE);
-
-    matmul_gpu_shared_mem_tiling<<<gridDim2_1, blockDim2_1, 0, stream>>>(d_op1_chunk, d_w2, d_op2_chunk, batch_size, N, OUTPUT_VEC);
-
-    dim3 blockDim2_2(BLOCK_SIZE);
-    dim3 gridDim2_2((OUTPUT_VEC + BLOCK_SIZE - 1)/BLOCK_SIZE);
-
-    add_bias<<<gridDim2_2, blockDim2_2, 0, stream>>>(d_op2_chunk, d_b2, batch_size, OUTPUT_VEC);
-
+    matmul_gpu_shared_mem_tiling<<<output_grid, matmul_block, 0, stream>>>(
+        d_op_chunk[LAYERS - 2], d_w[LAYERS - 1], d_op_chunk[LAYERS - 1],
+        d_b[LAYERS - 1], batch_size, N, OUTPUT_VEC);
 }
 
 int main() {
 
-    float *h_ip, *h_w1, *h_b1, *h_w2, *h_b2, *h_op;
-    float *d_ip, *d_w1, *d_b1 , *d_op1, *d_w2, *d_b2, *d_op2;
+    float *h_ip;
+    float *h_w[LAYERS];
+    float *h_b[LAYERS];
+    float *h_op;
 
+    float *d_ip;
+    float *d_w[LAYERS];
+    float *d_b[LAYERS];
+    float *d_op[LAYERS]; 
 
-
+    int size_w[LAYERS];
+    int size_b[LAYERS];
+    int size_op[LAYERS];
 
     int size_ip = BATCH_SIZE * INPUT_VEC * sizeof(float);
-    int size_w1 = INPUT_VEC * N * sizeof(float);
-    int size_b1 = N * sizeof(float);
-    int size_op1 = BATCH_SIZE * N * sizeof(float);
-    int size_w2 = N * OUTPUT_VEC * sizeof(float);
-    int size_op2 = BATCH_SIZE * OUTPUT_VEC * sizeof(float);
-    int size_b2 = OUTPUT_VEC * sizeof(float);
+
+    size_w[0] = INPUT_VEC * N * sizeof(float);
+    size_b[0] = N * sizeof(float);
+    size_op[0] = BATCH_SIZE * N * sizeof(float);
+
+    for(int i = 0; i < LAYERS-2; i++){
+        size_w[1+i] = N * N * sizeof(float);
+        size_b[1+i] = N * sizeof(float);
+        size_op[1+i] = BATCH_SIZE * N * sizeof(float);
+    }
+    
+    size_w[LAYERS - 1] = N * OUTPUT_VEC * sizeof(float);
+    size_b[LAYERS - 1] = OUTPUT_VEC * sizeof(float);
+    size_op[LAYERS - 1] = BATCH_SIZE * OUTPUT_VEC * sizeof(float);
+    
 
     if(BATCH_SIZE % STREAMS != 0){
         printf("Batch size is not divisible by number of streams");
@@ -143,34 +159,36 @@ int main() {
     int batch_size = BATCH_SIZE/STREAMS;
 
     h_ip = (float*)malloc(size_ip);
-    h_w1 = (float*)malloc(size_w1);
-    h_b1 = (float*)malloc(size_b1);
-    h_w2 = (float*)malloc(size_w2);
-    h_b2 = (float*)malloc(size_b2);
-    h_op = (float*)malloc(size_op2);
+    for (int i = 0; i < LAYERS; i++){
+        h_w[i] = (float*)malloc(size_w[i]);
+        h_b[i] = (float*)malloc(size_b[i]);
+    }
+    h_op = (float*)malloc(size_op[LAYERS-1]);
 
     srand(32);
     init_matrix(h_ip, BATCH_SIZE, INPUT_VEC);
-    init_matrix(h_w1, INPUT_VEC, N);
-    init_matrix(h_b1, 1, N);
-    init_matrix(h_w2, N, OUTPUT_VEC);
-    init_matrix(h_b2, 1, OUTPUT_VEC);
+    init_matrix(h_w[0], INPUT_VEC, N);
+    init_matrix(h_b[0], 1, N);
+    for(int i = 0; i < LAYERS-2; i++){
+        init_matrix(h_w[i+1], N, N);
+        init_matrix(h_b[i+1], 1, N);
+    }
+    init_matrix(h_w[LAYERS - 1], N, OUTPUT_VEC);
+    init_matrix(h_b[LAYERS - 1], 1, OUTPUT_VEC);
 
     cudaMalloc(&d_ip, size_ip);
-    cudaMalloc(&d_w1, size_w1);
-    cudaMalloc(&d_b1, size_b1);
-    cudaMalloc(&d_op1, size_op1);
-    cudaMalloc(&d_w2, size_w2);
-    cudaMalloc(&d_b2, size_b2);
-    cudaMalloc(&d_op2, size_op2);
+    for(int i = 0; i < LAYERS; i++ ){
+        cudaMalloc(&d_w[i], size_w[i]);
+        cudaMalloc(&d_b[i], size_b[i]);
+        cudaMalloc(&d_op[i], size_op[i]);
+    }
 
     cudaMemcpy(d_ip, h_ip, size_ip, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_w1, h_w1, size_w1, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b1, h_b1, size_b1, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_w2, h_w2, size_w2, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b2, h_b2, size_b2, cudaMemcpyHostToDevice);
-    cudaMemset(d_op1, 0, size_op1);
-    cudaMemset(d_op2, 0, size_op2);
+    for(int i = 0; i < LAYERS; i++){
+        cudaMemcpy(d_w[i], h_w[i], size_w[i], cudaMemcpyHostToDevice);
+        cudaMemcpy(d_b[i], h_b[i], size_b[i], cudaMemcpyHostToDevice);
+        cudaMemset(d_op[i], 0, size_op[i]);
+    }
 
     // create the streams
     cudaStream_t streams[STREAMS];
@@ -184,10 +202,13 @@ int main() {
     for(int stream_idx = 0; stream_idx < STREAMS; stream_idx++){
         int batch_offset = stream_idx * batch_size;
         float *d_ip_chunk = d_ip + batch_offset * INPUT_VEC; 
-        float *d_op1_chunk = d_op1 + batch_offset * N;
-        float *d_op2_chunk = d_op2 + batch_offset * OUTPUT_VEC;
-    
-        launch_mlp_chunk(d_ip_chunk, d_w1, d_b1, d_op1_chunk, d_w2, d_b2, d_op2_chunk, batch_size, streams[stream_idx]);
+        float *d_op_chunk[LAYERS];
+        for(int i = 0; i < LAYERS - 1; i++) {
+            d_op_chunk[i] = d_op[i] + batch_offset * N;
+        }
+        d_op_chunk[LAYERS - 1] = d_op[LAYERS - 1] + batch_offset * OUTPUT_VEC;
+        
+        launch_mlp_chunk(d_ip_chunk, d_w, d_b, d_op_chunk, batch_size, streams[stream_idx]);
     
     }
     
@@ -201,26 +222,26 @@ int main() {
         cudaStreamDestroy(streams[stream_idx]);
     }
 
-    cudaMemcpy(h_op, d_op2, size_op2, cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_op, d_op[LAYERS - 1], size_op[LAYERS - 1], cudaMemcpyDeviceToHost);
 
-    double elasped_time = end_time - start_time;
-    printf("Time: %f microseconds\n\n", (elasped_time * 1e6f));
+    double elapsed_time = end_time - start_time;
+    printf("Time: %f microseconds\n\n", (elapsed_time * 1e6f));
 
 
 
     free(h_ip);
-    free(h_w1);
-    free(h_b1);
-    free(h_w2);
-    free(h_b2);
+    for(int i = 0; i < LAYERS; i++){
+        free(h_w[i]);
+        free(h_b[i]);
+    }
     free(h_op);
 
     cudaFree(d_ip);
-    cudaFree(d_w1);
-    cudaFree(d_b1);   
-    cudaFree(d_op1);
-    cudaFree(d_w2);
-    cudaFree(d_b2);
-    cudaFree(d_op2);
+    for(int i = 0; i < LAYERS; i++){
+        cudaFree(d_w[i]);
+        cudaFree(d_b[i]);
+        cudaFree(d_op[i]);
+    }
 
+    return 0;
 }
